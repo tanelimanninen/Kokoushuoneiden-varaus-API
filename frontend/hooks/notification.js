015e66ce03a9b622c0d464bbbd8fe001c3cb3169
Taneli Manninen <taneli.manninen9@gmail.com>
2026-01-21 13:50:12 +0200

    Siistitään notification.js-tiedoston tyyliä

--- frontend/hooks/notification.js
+++ frontend/hooks/notification.js
@@ -1,32 +1,32 @@
 // ehdollistettu ja automatisoitu funktio popup-ilmoituksille
 function showAlert(message, type = "success", duration = 3000) {
-    // Haetaan DOM:sta oikea div-komponentti
-    const container = document.getElementById("alert-container");
+  // Haetaan DOM:sta oikea div-komponentti
+  const container = document.getElementById("alert-container");
 
-    // Luodaan komponentti notifikaation sisällölle
-    const alert = document.createElement("div");
-    alert.className = `custom-alert ${type}`;
+  // Luodaan komponentti notifikaation sisällölle
+  const alert = document.createElement("div");
+  alert.className = `custom-alert ${type}`;
 
-    // Notifikaation ikonikomponentti (ehdollistettu)
-    const icon = document.createElement("img");
-    icon.className = "alert-icon";
-    icon.src = type === "error" ? "../assets/circle-exclamation-solid-full.svg" : "../assets/circle-check-solid-full.svg";
-    icon.alt = type;
+  // Notifikaation ikonikomponentti (ehdollistettu)
+  const icon = document.createElement("img");
+  icon.className = "alert-icon";
+  icon.src = type === "error" ? "../assets/circle-exclamation-solid-full.svg" : "../assets/circle-check-solid-full.svg";
+  icon.alt = type;
 
-    // Notifikaation tekstikomponentti
-    const text = document.createElement("span");
-    text.textContent = message;
+  // Notifikaation tekstikomponentti
+  const text = document.createElement("span");
+  text.textContent = message;
+  
+  // Asetellaan komponentit parentteihinsa
+  alert.appendChild(icon);
+  alert.appendChild(text);
+  container.appendChild(alert);
 
-    // Asetellaan komponentit parentteihinsa
-    alert.appendChild(icon);
-    alert.appendChild(text);
-    container.appendChild(alert);
-
-    // Ajastetaan notifikaation näkyvyyden kesto (3s)
-    setTimeout(() => {
-        alert.style.opacity = "0";
-        setTimeout(() => alert.remove(), 300);
-    }, duration);
+  // Ajastetaan notifikaation näkyvyyden kesto (3s)
+  setTimeout(() => {
+      alert.style.opacity = "0";
+      setTimeout(() => alert.remove(), 300);
+  }, duration);
 }
 
 function showConfirm(message) {
