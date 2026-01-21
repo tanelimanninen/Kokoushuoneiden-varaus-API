9d199c8603808a7bad147dfd38736abe5361586a
Taneli Manninen <taneli.manninen9@gmail.com>
2026-01-21 10:39:57 +0200

    Lisää assets-kansioon uusi ikoni, päivitä main.js uudella vahvisnotifikaation custom-funktiolla ja päivitä komponenttien tyylit styles.css-tiedostossa

--- frontend/main.js
+++ frontend/main.js
@@ -75,7 +75,9 @@ async function loadReservations() {
 }
 
 async function deleteReservation(id) {
-  if (!confirm("Haluatko varmasti poistaa varauksen?")) return;
+ // Varmistetaan ensin käyttäjältä poistaminen
+ const confirmed = await showConfirm("Haluatko varmasti poistaa varauksen?");
+ if (!confirmed) return;
 
   const response = await fetch(`/api/reservations/${id}`, {
     method: "DELETE"
@@ -120,4 +122,59 @@ function showAlert(message, type = "success", duration = 3000) {
     alert.style.opacity = "0";
     setTimeout(() => alert.remove(), 300);
   }, duration);
+}
+
+function showConfirm(message) {
+  return new Promise((resolve) => {
+    // Haetaan DOM:sta oikea div-komponentti (käytetään samaa alert-komponenttia pohjana)
+    const container = document.getElementById("alert-container");
+    container.innerHTML = "";
+
+    // Luodaan koko sisällölle oma komponentti ja class
+    const confirm = document.createElement("div");
+    confirm.className = "custom-confirm";
+
+    // Luodaan ikonille ja tekstille oma div (layoutissa ylin rivi)
+    const content = document.createElement("div");
+    content.className = "confirm-content"
+    // Confirm ilmoituksen ikoni (käytetään alert-ikonin tyylittelyä)
+    const icon = document.createElement("img");
+    icon.className = "alert-icon";
+    icon.src = "./assets/circle-question-solid-full.svg";
+    icon.alt = "Vahvistus";
+    // Confirm ilmoituksen teksti
+    const text = document.createElement("span");
+    text.textContent = message;
+
+    // Confirm viestin painikkeille oma div (layoutissa alin rivi)
+    const buttons = document.createElement("div");
+    buttons.className = "confirm-buttons";
+    // Painike poiston vahvistukselle
+    const okBtn = document.createElement("button");
+    okBtn.textContent = "Vahvista";
+    okBtn.id = "confirm-ok";
+    // Painike poiston peruuttamiselle
+    const cancelBtn = document.createElement("button");
+    cancelBtn.textContent = "Peruuta";
+    cancelBtn.id = "confirm-cancel";
+    // Tapahtuman käsittely vahvistuspainikkeelle
+    okBtn.onclick = () => {
+      container.innerHTML = "";
+      resolve(true);
+    };
+    // Tapahtuman käsittely peruutuspainikkeelle
+    cancelBtn.onclick = () => {
+      container.innerHTML = "";
+      resolve(false);
+    };
+
+    // Asetellaan komponentit parentteihinsa
+    content.appendChild(icon);
+    content.appendChild(text);
+    buttons.appendChild(okBtn);
+    buttons.appendChild(cancelBtn);
+    confirm.appendChild(content);
+    confirm.appendChild(buttons);
+    container.appendChild(confirm);
+  })
 }
