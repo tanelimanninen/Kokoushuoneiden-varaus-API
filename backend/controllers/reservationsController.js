9d486d8a874ca27c65741f30d75f0b2fc8ca4157
Taneli Manninen <taneli.manninen9@gmail.com>
2026-01-21 15:54:06 +0200

    Lisää kommentteja reservationsController.js ja reservationService-tiedostoon

--- backend/controllers/reservationsController.js
+++ backend/controllers/reservationsController.js
@@ -1,27 +1,35 @@
 const service = require("../services/reservationService");
 
-/* POST /api/reservations */
+/* Luodaan POST-pyyntö osoitteessa /api/reservations */
 exports.createReservation = (req, res) => {
   try {
+    // Kutsutaan sevice-funktiota
     const reservation = service.createReservation(req.body);
+    // Palautetaan varaus status-koodilla 201 (created)
     res.status(201).json(reservation);
   } catch (err) {
+    // Palautetaan status-koodilla 400 (bad request) ja virheilmoitus
     res.status(400).json({ error: err.message });
   }
 };
 
-/* DELETE /api/reservations/:id */
+/* Luodaan DELETE-pyyntö osoitteessa /api/reservations/:id */
 exports.deleteReservation = (req, res) => {
   try {
+    // Kutsutaan sevice-funktiota
     service.deleteReservation(Number(req.params.id));
+    // Palautetaan varaus status-koodilla 204 (no content)
     res.status(204).send();
   } catch (err) {
+    // Palautetaan status-koodilla 404 (not found) ja virheilmoitus
     res.status(404).json({ error: err.message });
   }
 };
 
-/* GET /api/reservations/:room */
+/* Luodaan GET-pyyntö osoitteeseen /api/reservations/:room */
 exports.getReservationsByRoom = (req, res) => {
+  // Kutsutaan sevice-funktiota
   const reservations = service.getReservationsByRoom(req.params.room);
+  // Palautetaan varaukset JSON-muodossa
   res.json(reservations);
 };
--- backend/services/reservationService.js
+++ backend/services/reservationService.js
@@ -1,27 +1,32 @@
 const db = require("../data/inMemoryDb");
 const { isOverlapping } = require("../utils/timeUtils");
 
-/* Luo uusi varaus */
+/* FUNKTIO 1: Luo uusi varaus */
 exports.createReservation = ({ room, startTime, endTime }) => {
   // Validointi 1: tiedot puuttuvat
   if (!room || !startTime || !endTime) {
+    // Palautetaan virheviesti
     throw new Error("Puuttuvat tiedot");
   }
 
+  // Muutetaan JavaScript-olioiksi
   const start = new Date(startTime);
   const end = new Date(endTime);
   const now = new Date();
 
   // Validointi 2: Aloitusaika on menneisyydessä
   if (start < now) {
+    // Palautetaan virheviesti
     throw new Error("Varauksen aloitusaika ei voi olla menneessä");
   }
 
   // Validointi 3: Aloitusaika on ennen lopetusta
   if (start >= end) {
+    // Palautetaan virheviesti
     throw new Error("Varauksen aloitusaika tulee olla ennen lopetusaikaa");
   }
 
+  // Käydään tietokanta läpi, palauttaa true jos saman huone-arvon objektilla päällekkäinen varaus
   const overlapping = db.reservations.some(r =>
     r.room === room &&
     isOverlapping(startTime, endTime, r.startTime, r.endTime)
@@ -29,9 +34,11 @@ exports.createReservation = ({ room, startTime, endTime }) => {
 
   // Validointi 4: Varaus on päällekkäinen jo olemassa olevan varauksen kanssa
   if (overlapping) {
+    // Palautetaan virheviesti
     throw new Error("Aikaväli on jo varattu");
   }
 
+  // Luodaan varausobjekti
   const reservation = {
     id: db.nextId++,
     room,
@@ -39,22 +46,33 @@ exports.createReservation = ({ room, startTime, endTime }) => {
     endTime
   };
 
+  // Viedään objekti tietokantaan
   db.reservations.push(reservation);
+
+  // Palautetaan luotu varausobjekti
   return reservation;
 };
 
-/* Poista yksittäinen varaus */
+/* FUNKTIO 2: Poista yksittäinen varaus */
 exports.deleteReservation = (id) => {
+  // Haetaan tietokannasta parametria vastaava varausobjektin id-kenttä
   const index = db.reservations.findIndex(r => r.id === id);
 
+  // Jos varausta ei löydy
   if (index === -1) {
+    // Palautetaan virheviesti
     throw new Error("Varausta ei löytynyt");
   }
 
+  // Poistetaan varaus tietokannasta annetun indeksin perusteella
   db.reservations.splice(index, 1);
 };
 
-/* Hae huoneen varaukset */
+/* FUNKTIO 3: Hae annetun huoneen varaukset */
 exports.getReservationsByRoom = (room) => {
-  return db.reservations.filter(r => r.room === room);
+  // Haetaan tietokannasta huonekohtaiset varaukset
+  const roomReservations = db.reservations.filter(r => r.room === room);
+
+  // Palautetaan annetun huoneen varaukset
+  return roomReservations;
 };
