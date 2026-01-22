63e6453cc022a6df0c1a940010dc303f143b9ef2
Taneli Manninen <taneli.manninen9@gmail.com>
2026-01-22 09:51:20 +0200

    Päivitä muistitietokantapohjaa

--- backend/data/inMemoryDb.js
+++ backend/data/inMemoryDb.js
@@ -1,4 +1,14 @@
+// Alustetaan dataan huonearvot
+const rooms = [
+  { id: 1, name: "Kokoustila A" },
+  { id: 2, name: "Kokoustila B" },
+  { id: 3, name: "Kokoustila C" }
+];
+
+// Alustetaan dataan array varauksille
+const reservations = [];
+
 module.exports = {
-  reservations: [],
-  nextId: 1
+  rooms,
+  reservations
 };
