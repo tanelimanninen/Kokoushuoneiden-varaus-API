d44ca8a278c27ce06a76136f4ba70917226e8668
Taneli Manninen <taneli.manninen9@gmail.com>
2026-01-22 14:19:03 +0200

    Siirrä main.js-tiedoston deleteReservation-funktio omaan js-tiedostoon

---
+++ frontend/hooks/deleteReservation.js
@@ -0,0 +1,36 @@
+import { showConfirm } from "./showConfirm";
+import { showAlert } from "./showAlert";
+import { loadReservations } from "./loadReservations";
+
+// Varausten poistaminen käyttöliittymässä (DELETE)
+async function deleteReservation(id) {
+  // Varmistetaan poistaminen ensin käyttäjältä
+  const confirmed = await showConfirm("Haluatko varmasti poistaa varauksen?");
+  
+  // Jos ei varmistusta, lopetetaan tähän
+  if (!confirmed) return;
+
+  // Haetaan palvelimelta id-arvoa vastaava varaus ja lähetetään DELETE-pyyntö
+  const response = await fetch(`/api/reservations/${id}`, {
+    method: "DELETE"
+  });
+
+  // Jos status koodi on 204 (no content)
+  if (response.status === 204) {
+    // Tulostetaan käyttöliittymään notifikaatio (success)
+    showAlert("Varaus poistettu", "success");
+    // Päivitetään listakomponentti
+    loadReservations();
+  }
+  // Jos status koodi on muuta kuin 204 
+  else {
+    // Luodaan palvelimen datasta JavaScript-olio
+    const err = await response.json();
+    // Tulostetaan data konsolissa
+    console.log(err);
+    // Tulostetaan käyttöliittymään notifikaatio (error)
+    showAlert("Varauksen poisto epäonnistui", "error");
+  }
+}
+
+export { deleteReservation };
--- frontend/hooks/main.js
+++ frontend/hooks/main.js
@@ -1,40 +1,6 @@
-import { showAlert } from "./showAlert.js";
-import { showConfirm } from "./showConfirm.js";
-
 import { createReservation } from "./createReservation.js";
 import { loadReservations } from "./loadReservations.js";
 
 // Haetaan DOM:ista painikkeet ja lisätään tapahtuman käsittelijät
 document.getElementById("createBtn").addEventListener("click", createReservation);
 document.getElementById("searchBtn").addEventListener("click", loadReservations);
-
-// Varausten poistaminen käyttöliittymässä (DELETE)
-async function deleteReservation(id) {
-  // Varmistetaan poistaminen ensin käyttäjältä
-  const confirmed = await showConfirm("Haluatko varmasti poistaa varauksen?");
-  
-  // Jos ei varmistusta, lopetetaan tähän
-  if (!confirmed) return;
-
-  // Haetaan palvelimelta id-arvoa vastaava varaus ja lähetetään DELETE-pyyntö
-  const response = await fetch(`/api/reservations/${id}`, {
-    method: "DELETE"
-  });
-
-  // Jos status koodi on 204 (no content)
-  if (response.status === 204) {
-    // Tulostetaan käyttöliittymään notifikaatio (success)
-    showAlert("Varaus poistettu", "success");
-    // Päivitetään listakomponentti
-    loadReservations();
-  }
-  // Jos status koodi on muuta kuin 204 
-  else {
-    // Luodaan palvelimen datasta JavaScript-olio
-    const err = await response.json();
-    // Tulostetaan data konsolissa
-    console.log(err);
-    // Tulostetaan käyttöliittymään notifikaatio (error)
-    showAlert("Varauksen poisto epäonnistui", "error");
-  }
-}
