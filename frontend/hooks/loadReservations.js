09782ec9696152b7bf9225f11e0226424f608200
Taneli Manninen <taneli.manninen9@gmail.com>
2026-01-22 14:14:53 +0200

    Siirrä main.js-tiedoston loadReservations-funktio omaan js-tiedostoon

---
+++ frontend/hooks/loadReservations.js
@@ -0,0 +1,52 @@
+import { formatDate } from "./formatDate.js";
+// import deleteReservation
+
+// Varausten tulostaminen käyttöliittymässä (GET)
+async function loadReservations() {
+  // Haetaan DOM:ista huoneen valintakentän arvo
+  const room = document.getElementById("searchRoom").value;
+  // Haetaan DOM:ista lista-komponentti
+  const list = document.getElementById("reservations");
+  list.innerHTML = "";
+
+  // Haetaan palvelimelta huonekohtainen data
+  const response = await fetch(`/api/reservations/${room}`);
+  // Luodaan datasta JavaScript-olio
+  const reservations = await response.json();
+
+  // Jos varauksia ei löydy
+  if (reservations.length === 0) {
+    // Tulostetaan listakomponenttiin li-elementti
+    list.innerHTML = "<li>Ei varauksia</li>";
+    return;
+  }
+
+  // Käydään reservations-array läpi ja lisätään jokaiseen seuraavat
+  reservations.forEach(r => {
+    // Luodaan li-elementti
+    const li = document.createElement("li");
+    // Sisällytetään apufunktioiden avulla li-komponenttiin varauksen aikatiedot
+    li.textContent = `${formatDate(r.startTime)} – ${formatDate(r.endTime)}`;
+
+    // Luodaan poistopainike
+    const deleteBtn = document.createElement("button");
+    // Luodaan painikkeelle ikoni
+    const icon = document.createElement("img");
+    // Haetaan ikoni projektin hakemistosta
+    icon.src = "./assets/circle-xmark-solid-full.svg"
+    // Lisätään ikonille alt-teksti
+    icon.alt = "Poista varaus";
+    // Lisätään ikonikomponentille class-arvo
+    icon.className = "delete-icon";
+
+    // Asetellaan komponentit parentteihinsa
+    deleteBtn.appendChild(icon);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+
+    // Määritellään poistopainikkeen tapahtuman käsittelijä
+    deleteBtn.onclick = () => deleteReservation(r.id);
+  });
+}
+
+export { loadReservations };
--- frontend/hooks/main.js
+++ frontend/hooks/main.js
@@ -1,58 +1,12 @@
 import { showAlert } from "./showAlert.js";
 import { showConfirm } from "./showConfirm.js";
-import { formatDate } from "./formatDate.js";
+
+import { createReservation } from "./createReservation.js";
+import { loadReservations } from "./loadReservations.js";
 
 // Haetaan DOM:ista painikkeet ja lisätään tapahtuman käsittelijät
 document.getElementById("createBtn").addEventListener("click", createReservation);
 document.getElementById("searchBtn").addEventListener("click", loadReservations);
-
-// Varausten tulostaminen käyttöliittymässä (GET)
-async function loadReservations() {
-  // Haetaan DOM:ista huoneen valintakentän arvo
-  const room = document.getElementById("searchRoom").value;
-  // Haetaan DOM:ista lista-komponentti
-  const list = document.getElementById("reservations");
-  list.innerHTML = "";
-
-  // Haetaan palvelimelta huonekohtainen data
-  const response = await fetch(`/api/reservations/${room}`);
-  // Luodaan datasta JavaScript-olio
-  const reservations = await response.json();
-
-  // Jos varauksia ei löydy
-  if (reservations.length === 0) {
-    // Tulostetaan listakomponenttiin li-elementti
-    list.innerHTML = "<li>Ei varauksia</li>";
-    return;
-  }
-
-  // Käydään reservations-array läpi ja lisätään jokaiseen seuraavat
-  reservations.forEach(r => {
-    // Luodaan li-elementti
-    const li = document.createElement("li");
-    // Sisällytetään apufunktioiden avulla li-komponenttiin varauksen aikatiedot
-    li.textContent = `${formatDate(r.startTime)} – ${formatDate(r.endTime)}`;
-
-    // Luodaan poistopainike
-    const deleteBtn = document.createElement("button");
-    // Luodaan painikkeelle ikoni
-    const icon = document.createElement("img");
-    // Haetaan ikoni projektin hakemistosta
-    icon.src = "./assets/circle-xmark-solid-full.svg"
-    // Lisätään ikonille alt-teksti
-    icon.alt = "Poista varaus";
-    // Lisätään ikonikomponentille class-arvo
-    icon.className = "delete-icon";
-
-    // Asetellaan komponentit parentteihinsa
-    deleteBtn.appendChild(icon);
-    li.appendChild(deleteBtn);
-    list.appendChild(li);
-
-    // Määritellään poistopainikkeen tapahtuman käsittelijä
-    deleteBtn.onclick = () => deleteReservation(r.id);
-  });
-}
 
 // Varausten poistaminen käyttöliittymässä (DELETE)
 async function deleteReservation(id) {
